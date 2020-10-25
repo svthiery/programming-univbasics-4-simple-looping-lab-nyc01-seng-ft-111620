c28f3f1f820d799a5f3971376abedbc7c6fb26d8
Sarah Kain Thiery <sarahvkain@gmail.com>
2020-10-25 22:30:03 +0000

    Automatically backed up by Learn

--- lib/simple_loops.rb
+++ lib/simple_loops.rb
@@ -24,7 +24,7 @@ end
 
 def return_string_array (array)
   counter = 0 
-  while counter <= array.length do
+  while counter < array.length do
     array[counter] = array[counter].to_s
     counter += 1 
   end
