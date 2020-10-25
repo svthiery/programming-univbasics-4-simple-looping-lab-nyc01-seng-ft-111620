bd47ce278e148cab9bbfec9f806b19066b85a510
Sarah Kain Thiery <sarahvkain@gmail.com>
2020-10-25 22:23:55 +0000

    Automatically backed up by Learn

--- lib/simple_loops.rb
+++ lib/simple_loops.rb
@@ -1,1 +1,24 @@
-# Write your methods here
+def loop_message_five_times (message)
+  counter = 0 
+  while counter <= 5 do
+    puts message
+    counter += 1
+  end
+end
+
+def loop_message_n_times (message, number)
+  counter = 0 
+  while counter <= number do 
+    puts message
+    counter += 1 
+  end
+end
+
+def output_array (array)
+  counter = 0 
+  while counter <= array.length do 
+    puts array[counter]
+    counter += 1 
+  end
+end
+
